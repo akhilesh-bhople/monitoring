ece5153701daaa51462537559ebd9608561180a7
root <root@in-ibmibm2254.persistent.co.in>
2018-06-02 10:15:48 +0530

    Updated variable names

--- monitor.sh
+++ monitor.sh
@@ -13,24 +13,26 @@ url_list=`echo $@`
 # Check and process if URL is valid
 validate_url()
 {
-	validity=`curl -L -I -s --retry 3 --connect-timeout 5 $1`
+	url=$1
+	validity=`curl -L -I -s --retry 3 --connect-timeout 5 $url`
 	if [ -n "$validity" ]
 	then
-		check_response $1
+		get_response $url
 	else
 		status_check="Invalid"
-		result $status_check $1 NA
+		result $status_check $url NA
 	fi
 }
 
 # Get response time and status from the URL
 get_response()
 {
-	response_time=$(curl -o /tmp/response -i -s -L -w %{time_total} $1)
+	url=$1
+	response_time=$(curl -o /tmp/response -i -s -L -w %{time_total} $url)
         status_code=`grep "HTTP/" /tmp/response | cut -d" " -f2`
         status_phrase=`grep -i "component status" /tmp/response | cut -d":" -f2`
         rm -rf /tmp/response
-	check_response $response_time $status_code $status_phrase $1
+	check_response $response_time $status_code $status_phrase $url
 }
 	
 # Check site status and response time
@@ -40,29 +42,33 @@ check_response()
 	echo $response_time
 	status_code=$2
 	status_phrase=`echo $3 | tr '[A-Z]' '[a-z]'`
+	url=$4
 	if [ "$status_code" -eq 200 ] && [ "$status_phrase" = "green" ]
 	then
 		status_check="Green"
-		result $status_check $4 $response_time
+		result $status_check $url $response_time
 	else
 		status_check="Red"
-		result $status_check $4 $response_time
+		result $status_check $url $response_time
 	fi
 
-	if [ "$response_time" -gt 300 ]
-	then
-		echo "Slow response"
-	else
-		echo "Fast response"
-	fi
+#	if [ "$response_time" -gt 300 ]
+#	then
+#		echo "Slow response"
+#	else
+#		echo "Fast response"
+#	fi
 }
 
 # Process results
 result()
 {
+	status_check=$1
+	url=$2
+	response_time=$3
 	timestamp=`date +%s`
-	printf '%10s %10s %15s %45s\n' $timestamp $1 $3 $2
-	echo $timestamp,$1,$2,$3 >>/var/log/monitoring.log
+	printf '%10s %10s %15s %45s\n' $timestamp $status_check $response_time $url
+	echo $timestamp,$status_check,$url,$response_time >>/var/log/monitoring.log
 }
 
 table_header()
@@ -77,12 +83,12 @@ table_footer()
 	echo "---------------------------------------------------------------------------------------------------------------------------------------------"
 }
 
-check_response 3.00 200 GreeN 
-#table_header
-#for url in $url_list
-#do
-#	validate_url $url
-#done
-#table_footer
+#check_response 3.00 200 GreeN 
+table_header
+for url in $url_list
+do
+	validate_url $url
+done
+table_footer
 
 
